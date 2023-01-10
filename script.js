f89fe1b3bb6a1b8e49bac536115764951dc029b5
rickyl3 <113804998+rickyl3@users.noreply.github.com>
2023-01-10 13:47:56 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,29 @@
+// Ricky Leung
+// Period 7/8 Odd
+// Level 8 JS Classwork
+
+
+// Subchapter 2
+// var fs = require('fs');
+
+// var data = fs.readFileSync('input.txt');
+
+// console.log(data.toString());
+// console.log('Program Ended');
+
+// fs.readFile('input.txt', function (err, data) {
+//     if (err) {
+//         return console.error(err);
+//     }
+//     console.log(data.toString());
+// });
+// console.log('Program Ended');
+
+
+// Subchapter 3
+// var http = require('http');
+// http.createServer(function (request, response) {
+//     response.writeHead(200, {'Content-Type': 'text/plain'});
+//     response.end('Hello World\n');
+// }).listen(8081);
+// console.log('Server running at http://127.0.0.1:8081/');
---
+++ webserver.js
@@ -0,0 +1,28 @@
+// Ricky Leung
+// Period 7/8 Odd
+// Level 8 JS Classwork
+
+
+
+// Subchapter 4
+var http = require('http');
+var fs = require('fs');
+var url = require('url');
+
+http.createServer(function (request, response) {
+    var pathname = url.parse(request.url).pathname.substring(1);
+    console.log('Request for ' + pathname + ' received.');
+    fs.readFile(pathname, function (err, data) {
+        if (err) {
+            console.log(err);
+            response.writeHead(404, {'Content-Type': 'text/html'});
+        } else {
+            response.writeHead(200, {'Content-Type': 'text/html'});
+            response.write(data.toString());
+        }
+        response.end();
+    });
+}).listen(8081);
+console.log('Server running at http://127.0.0.1:8081');
+
+// Go to http://127.0.0.1:8081/index.html
